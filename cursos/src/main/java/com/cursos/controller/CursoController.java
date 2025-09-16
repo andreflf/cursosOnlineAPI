4a88f4380a282db2dd293719d674c24bb385e858
andre <andre@DESKTOP-M0D33BE>
2025-09-16 08:55:06 -0300

    refactor: adiciona anotação na classe CursoService e CursoController

--- cursos/src/main/java/com/cursos/controller/CursoController.java
+++ cursos/src/main/java/com/cursos/controller/CursoController.java
@@ -1,5 +1,10 @@
 package com.cursos.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/cursos")
 public class CursoController {
 	
 
--- cursos/src/main/java/com/cursos/service/CursoService.java
+++ cursos/src/main/java/com/cursos/service/CursoService.java
@@ -1,5 +1,8 @@
 package com.cursos.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CursoService {
 
 }
